9bd3a5df7c5501637de3c1c6b164406e03813bdf
Dhivagaran <dhivagaran2004@gmail.com>
2026-07-01 21:16:11 +0530

    phase 5

---
+++ Backend/App/database/databse.py
@@ -0,0 +1,13 @@
+from sqlalchemy import engine
+from sqlalchemy.orm import sessionmaker,declarative_base
+from App.config import DATABASE_URL
+
+engine =create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+Base = declarative_base()
