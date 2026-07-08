99e037a9bd8f293644c7125a0b01b9e5373e3e3e
Dhivagaran <dhivagaran2004@gmail.com>
2026-07-08 20:48:55 +0530

    login auth.py2.0

--- Backend/App/routes/auth.py
+++ Backend/App/routes/auth.py
@@ -5,7 +5,8 @@ from App.database.database import get_db
 from App.models.user import User
 from App.schema.schema import UserCreate, UserResponse
 from App.auth.auth import hash_password
-
+from App.schema.schema import (UserCreate, UserResponse,UserLogin,Token)
+from App.auth.auth import ( hash_password, verify_password, create_access_token)
 router = APIRouter(
     prefix="/auth",
     tags=["Authentication"]
@@ -20,7 +21,6 @@ def register_user(
     user: UserCreate,
     db: Session = Depends(get_db)
 ):
-
     existing_user = (
         db.query(User)
         .filter(User.email == user.email)
@@ -44,3 +44,44 @@ def register_user(
     db.refresh(new_user)
 
     return new_user
+
+@router.post(
+    "/login",
+    response_model=Token
+)
+def login_user(
+    user: UserLogin,
+    db: Session = Depends(get_db)
+):
+
+    db_user = (
+        db.query(User)
+        .filter(User.email == user.email)
+        .first()
+    )
+
+    if not db_user:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid email or password"
+        )
+
+    if not verify_password(
+        user.password,
+        db_user.password_hash
+    ):
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid email or password"
+        )
+
+    access_token = create_access_token(
+        {
+            "sub": db_user.email
+        }
+    )
+
+    return {
+        "access_token": access_token,
+        "token_type": "bearer"
+    }
